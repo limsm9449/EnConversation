934a23c9262f19acddd677e6699aeca29100fcc9
limsm9449 <limsm9449@naver.com>
2016-12-22 14:24:11 +0900

    2016.12.22

--- app/src/main/java/com/sleepingbear/enconversation/MainActivity.java
+++ app/src/main/java/com/sleepingbear/enconversation/MainActivity.java
@@ -501,7 +501,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent msg = new Intent(Intent.ACTION_SEND);
             msg.addCategory(Intent.CATEGORY_DEFAULT);
             msg.putExtra(Intent.EXTRA_SUBJECT, "최고의 영어회화 어플");
-            msg.putExtra(Intent.EXTRA_TEXT, "영어회화 어플을 사용해보세요.");
+            msg.putExtra(Intent.EXTRA_TEXT, "영어회화.. 참 어렵죠? 영어회화에 도움이 되는 '최고의 영어회화' 어플을 사용해 보세요. https://play.google.com/store/apps/details?id=com.sleepingbear.enconversation ");
             msg.setType("text/plain");
             startActivity(Intent.createChooser(msg, "어플 공유"));
         }
